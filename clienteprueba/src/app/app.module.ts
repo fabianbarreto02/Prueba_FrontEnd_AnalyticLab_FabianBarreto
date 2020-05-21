73d46fc7fedf65d9fcfca54e704558f701be6a81
FABIAN DAVID BARRETO SANCHEZ <fdbarretos@correo.udistrital.edu.co>
2020-05-21 14:34:31 -0500

    Se crea el componente para manejar el mapbox y se ajusta tarjetas para el body

--- clienteprueba/src/app/app.module.ts
+++ clienteprueba/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BodyComponent } from './components/body/body.component';
+import { MapboxComponent } from './components/mapbox/mapbox.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BodyComponent
+    BodyComponent,
+    MapboxComponent
   ],
   imports: [
     BrowserModule,
