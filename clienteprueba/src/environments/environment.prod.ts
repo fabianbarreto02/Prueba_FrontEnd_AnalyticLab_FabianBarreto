b1997beb69c1f389ea4d7ce4561a83e9410c587e
FABIAN DAVID BARRETO SANCHEZ <fdbarretos@correo.udistrital.edu.co>
2020-05-26 10:09:59 -0500

    Se ajusta firebase por cambio de url del api, y se ajusta readme

--- clienteprueba/src/environments/environment.prod.ts
+++ clienteprueba/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   mapboxkey: 'pk.eyJ1IjoiZmRiYXJyZXRvcyIsImEiOiJja2FoNjNpZXQwYTk1MnJsbGMxanh2MGVuIn0.3bIpH0seDLoksWBARhJZWg',
-  API_SERVICE: 'https://34.71.9.156/'
+  API_SERVICE: 'https://alw-lab.herokuapp.com//'
 };
